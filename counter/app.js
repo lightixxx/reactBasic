3d3cbed1f8c34532c19a817e76c833ea11a7b687
Ki Woong Lee <59480963+lightixxx@users.noreply.github.com>
2020-11-26 20:39:40 +0900

    js 리팩토링

--- counter/app.js
+++ counter/app.js
@@ -2,18 +2,28 @@ const number = document.querySelector('#number'),
       decrease = document.querySelector('#decrease'),
       increase = document.querySelector('#increase');
 
-let num = number.innerText;
-num = Number(num);
+// let num = number.innerText;
+// num = Number(num);
 
-function handleDecrease() {
-  num--;
-  number.innerText = num;
-}
+// function handleDecrease() {
+//   num--;
+//   number.innerText = num;
+// }
 
-function handleIncrease() {
-  num++;
-  number.innerText = num;
-}
+// function handleIncrease() {
+//   num++;
+//   number.innerText = num;
+// }
 
-decrease.addEventListener('click', handleDecrease);
-increase.addEventListener('click', handleIncrease);
+// decrease.addEventListener('click', handleDecrease);
+// increase.addEventListener('click', handleIncrease);
+
+increase.onclick = () => {
+  const current = parseInt(number.innerText, 10);
+  number.innerText = current + 1;
+};
+
+decrease.onclick = () => {
+  const current = parseInt(number.innerText, 10);
+  number.innerText = current - 1;
+};
