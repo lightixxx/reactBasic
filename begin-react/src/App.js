b3a60aaeee28492e8379a1720e1b9518722f7d46
Ki Woong Lee <59480963+lightixxx@users.noreply.github.com>
2020-12-08 17:21:39 +0900

    make first component

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Hello from './Hello';
+import './App.css';
 
 function App() {
+  const name = 'Lightix';
+  const style = {
+    backgroundColor: 'black',
+    color: '#0066ff',
+    fontSize: 24,
+    padding: '1rem 2rem'
+  }
+  
   return (
-    <div>
-      <Hello />
-    </div>
+    <>
+      {/* 주석 작성법1 */}
+      <Hello
+        //주석 작성법2 
+      />
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </>
   );
 }
 
--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -2,7 +2,7 @@
 import React from 'react';
 
 function Hello() {
-  return <div>안녕하세요!</div>
+  return <div>Hello World!</div>
 }
 
 // Hello 컴포넌트를 내보내기
