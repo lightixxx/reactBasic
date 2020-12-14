1c2aafd445e8c8b96903ab85876bf96ba0cbd3fa
Ki Woong Lee <59480963+lightixxx@users.noreply.github.com>
2020-12-14 18:20:37 +0900

    Counter 예제 완성

--- begin-react/src/Counter.js
+++ begin-react/src/Counter.js
@@ -12,10 +12,12 @@ function Counter() {
   }
 
   return (
-    <div>
+    <div className="wrapper">
       <h1>{number}</h1>
-      <button onClick={onIncrease}>+1</button>
-      <button onClick={onDecrease}>-1</button>
+      <div>
+        <button onClick={onIncrease}>+1</button>
+        <button onClick={onDecrease}>-1</button>
+      </div>
     </div>
   )
 }
